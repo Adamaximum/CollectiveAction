1d1279118fa57f81875d237fd456b9701900f88e
Adam Goren <amg1090@nyu.edu>
2019-09-26 16:42:41 -0400

    Set up basic movement functions

---
+++ Assets/Scripts/MagnetControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetControl : MonoBehaviour
+{
+    public float movementSpeed = 0.2f;
+
+    float movementX;
+    float movementY;
+
+    [Header("Positive Borders")]
+    public float borderRight = 7.7f;
+    public float borderUp = 4f;
+    [Header("Negative Borders (Do Not Edit)")]
+    public float borderLeft;
+    public float borderDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        borderLeft = borderRight * -1;
+        borderDown = borderUp * -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.tag == "Horizontal")
+        {
+            if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) && transform.position.x > borderLeft)
+            {
+                movementX = -movementSpeed;
+            }
+            else if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) && transform.position.x < borderRight)
+            {
+                movementX = movementSpeed;
+            }
+            else
+            {
+                movementX = 0;
+            }
+        }
+        if (gameObject.tag == "Vertical")
+        {
+            if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) && transform.position.y > borderDown)
+            {
+                movementY = -movementSpeed;
+            }
+            else if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && transform.position.y < borderUp)
+            {
+                movementY = movementSpeed;
+            }
+            else
+            {
+                movementY = 0;
+            }
+        }
+        transform.position += new Vector3(movementX, movementY, 0f);
+    }
+}
