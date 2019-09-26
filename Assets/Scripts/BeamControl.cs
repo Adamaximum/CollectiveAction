ad7cdb3535a7ff145cada2031021566a397ae594
Adam Goren <amg1090@nyu.edu>
2019-09-26 17:15:08 -0400

    Added some new elements to scene

---
+++ Assets/Scripts/BeamControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamControl : MonoBehaviour
+{
+    public SpriteRenderer beamSR;
+    public Transform vertTR;
+    public Transform horizTR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beamSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
